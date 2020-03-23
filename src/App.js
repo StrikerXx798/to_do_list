9e45f507fb04490edf9fdfb2f43ae7f06fc91c59
StrkerXx798 <str1k4@yandex.ru>
2020-03-23 21:21:49 +0300

    changeStatus & changeFilter add

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import TodoListFooter from "./TodoListFooter";
 import TodoListTasks from "./TodoListTasks";
 
 class App extends React.Component {
-    newTaskTitleRef = React.createRef();
-
     state = {
         tasks: [
             {title: "JS", isDone: true, priority: "medium"},
@@ -18,16 +16,32 @@ class App extends React.Component {
         filterValue: "Completed"
     };
 
-    onAddTaskClick = () => {
-        let newText = this.newTaskTitleRef.current.value;
-        this.newTaskTitleRef.current.value = '';
+    addText = (newText) => {
         let newTask = {
             title: newText,
             isDone: true,
             priority: "low"
         };
-        let newTasks =
-            [...this.state.tasks, newTask];
+        let newTasks = [...this.state.tasks, newTask];
+        this.setState({
+            tasks: newTasks
+        });
+    }
+
+    changeFilter = (newFilterValue) => {
+        this.setState({
+            filterValue: newFilterValue
+        })
+    }
+
+    changeStatus = (task, isDone) => {
+        let newTasks = this.state.tasks.map(t => {
+            if (t != task) {
+                return t;
+            } else {
+                return {...t, isDone: isDone}
+            }
+        });
         this.setState({
             tasks: newTasks
         });
@@ -37,9 +51,19 @@ class App extends React.Component {
         return (
             <div className="App">
                 <div className="todoList">
-                    <TodoListHeader onAddTaskClick = {this.onAddTaskClick} newTaskTitleRef = {this.newTaskTitleRef} />
-                    <TodoListTasks tasks = {this.state.tasks}/>
-                    <TodoListFooter filterValue = {this.state.filterValue}/>
+                    <TodoListHeader addText = {this.addText} newTaskTitleRef = {this.newTaskTitleRef} />
+                    <TodoListTasks changeStatus = {this.changeStatus} tasks = {this.state.tasks.filter(t => {
+                        if (this.state.filterValue === "All") {
+                            return true
+                        }
+                        if (this.state.filterValue === "Active") {
+                            return t.isDone === false
+                        }
+                        if (this.state.filterValue === "Completed") {
+                            return t.isDone === true
+                        }
+                    })}/>
+                    <TodoListFooter filterValue = {this.state.filterValue} changeFilter = {this.changeFilter}/>
                 </div>
             </div>
         );
--- src/TodoListTask.jsx
+++ src/TodoListTask.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 class TodoListTask extends React.Component {
+    onIsDOneChanged = (e) => {
+        this.props.changeStatus(this.props.task, e.currentTarget.checked)
+    }
     render = (props) => {
         return (
             <div className="todoList-task">
-                <input type="checkbox" checked={this.props.isDone}/>
+                <input
+                    type="checkbox"
+                    checked={this.props.task.isDone}
+                    onChange={this.onIsDOneChanged}
+                />
                 <span>{this.props.title}, {this.props.priority}</span>
             </div>
         );
