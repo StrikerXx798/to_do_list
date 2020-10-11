abe18119819ec74f0a96c8cbae30ed0b4f2a45d6
StrikerXx798 <str1k4@yandex.ru>
2020-10-12 00:57:34 +0300

    Updates

---
+++ precache-manifest.85019a012d861330cd3ae3aa5a3779b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0664fa7445a4c5d64ffc18ba6ca5f81",
+    "url": "/to_do_list/index.html"
+  },
+  {
+    "revision": "b9375a2faaffe853a67d",
+    "url": "/to_do_list/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "ce9e765d0f623136f044",
+    "url": "/to_do_list/static/js/2.47819b3e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/to_do_list/static/js/2.47819b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9375a2faaffe853a67d",
+    "url": "/to_do_list/static/js/main.3878db1d.chunk.js"
+  },
+  {
+    "revision": "44539955729a6723fe61",
+    "url": "/to_do_list/static/js/runtime-main.370ef160.js"
+  }
+]);
