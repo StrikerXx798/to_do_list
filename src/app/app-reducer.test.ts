98f245b6f3d57e4fce9ba3badbcdd05c669895b3
StrikerXx798 <str1k4@yandex.ru>
2020-10-12 16:30:26 +0300

    Errors and loading

--- src/app/app-reducer.test.ts
+++ src/app/app-reducer.test.ts
@@ -1,24 +1,25 @@
-import {appReducer, InitialStateType, setErrorAC, setStatusAC} from "./app-reducer";
+import {appReducer, InitialStateType, setAppErrorAC, setAppStatusAC} from './app-reducer'
 
-let startState: InitialStateType
+let startState: InitialStateType;
 
 beforeEach(() => {
     startState = {
-        status: 'idle',
-        error: null
+        error: null,
+        status: 'idle'
     }
 })
 
-test('correct error message', () => {
+test('correct error message should be set', () => {
 
-    const endState = appReducer(startState, setErrorAC('some error'))
+    const endState = appReducer(startState, setAppErrorAC('some error'))
 
-    expect(endState.error).toBe('some error')
+    expect(endState.error).toBe('some error');
 })
 
-test('correct status', () => {
+test('correct status should be set', () => {
 
-    const endState = appReducer(startState, setStatusAC('loading'))
+    const endState = appReducer(startState, setAppStatusAC('loading'))
 
-    expect(endState.status).toBe('loading')
+    expect(endState.status).toBe('loading');
 })
+
